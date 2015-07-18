e96faaafd0c36c9535924defe3a49336dca542b0
abbyhe <abbyhe@yahoo-inc.com>
2015-07-18 02:50:42 -0700

    late night

---
+++ ShotsApp-master/ShotsApp/StartController.swift
@@ -0,0 +1,30 @@
+//
+//  StartController.swift
+//  ShotsApp
+//
+//  Created by Xu Abby He on 7/18/15.
+//  Copyright (c) 2015 Yogesh Nagarur. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartController : UITabBarController {
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let item0 : UITabBarItem = self.tabBar.items?[0] as! UITabBarItem
+        var item1 : UITabBarItem = self.tabBar.items?[1] as! UITabBarItem
+        //var item2 : UITabBarItem = self.tabBar.items?[2] as! UITabBarItem
+        item0.title = "Create"
+        item1.title = "List"
+        //item2.title = "Explore"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
