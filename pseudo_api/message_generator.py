d9d563c98c9583d5ff7eeaa66302741e62c0b5b5
sanchuah <sanchuah@andrew.cmu.edu>
2015-07-17 21:38:54 -0700

    upload.js

--- pseudo_api/message_generator.py
+++ pseudo_api/message_generator.py
@@ -1,12 +1,19 @@
 import random
 import json
+import time
 def get_all():
     ret = []
     for i in xrange(10, 20+1):
         m = {}
-        m['mid'] = i
+        m['id'] = i
+        m['msg_type'] = 'pokemon'
         m['title'] = 'Pokemon %d' % i
+        m['doc'] = """Extremely we promotion remainder eagerness enjoyment an. Ham her demands removal brought minuter raising invited gay. Contented consisted continual curiosity contained get sex. Forth child dried in in aware do. You had met they song how feel lain evil near. Small she avoid six yet table china. And bed make say been then dine mrs. To household rapturous fulfilled attempted on so. """
+        m['url'] = 'https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/%d.png' %(i)
         m['img_url'] = 'https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/%d.png' %(i)
+        m['start_time'] = time.time()
+        m['end_time'] = time.time() + 86400
+        m['category'] = 'pokemon'
         m['lat'] = 37.4253498 + (random.random() / 10.0)
         m['lng'] = -122.0765002 + (random.random() / 10.0)
         if i > 15:
@@ -15,9 +22,6 @@ def get_all():
         else:
             m['lock'] = False
             m['unlock_type'] = None
-        m['category'] = 'pokemon'
-        m['message_type'] = 'pokemon'
-        m['desc'] = """Extremely we promotion remainder eagerness enjoyment an. Ham her demands removal brought minuter raising invited gay. Contented consisted continual curiosity contained get sex. Forth child dried in in aware do. You had met they song how feel lain evil near. Small she avoid six yet table china. And bed make say been then dine mrs. To household rapturous fulfilled attempted on so. """
         ret.append(m)
     return ret
 
@@ -34,6 +38,7 @@ def gen_message(dir_path):
         outf = open(filename, "w")
         print >> outf, json.dumps(pokemon, indent=2)
         outf.close()
+
 if __name__ == "__main__":
     gen_search_js("search.js")
     gen_message("message")
