c6e718477dea4f0908ee8055a1af959244f57c91
sanchuah <sanchuah@andrew.cmu.edu>
2015-07-17 20:51:11 -0700

    update search.js

---
+++ pseudo_api/search.js
@@ -0,0 +1,112 @@
+[
+    {
+        "mid": 1,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/11.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": false,
+        "unlock_type": null,
+        "category": "pokemon"
+    },
+    {
+        "mid": 2,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/12.png",
+        "lat": 37.5253498,
+        "lng": -122.0165002,
+        "time": 1437190740,
+        "lock": false,
+        "unlock_type": null,
+        "category": "pokemon"
+    },
+    {
+        "mid": 3,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/13.png",
+        "lat": 37.4259498,
+        "lng": -122.0763002,
+        "time": 1437190740,
+        "lock": true,
+        "unlock_type": "upload_pic",
+        "category": "pokemon"
+    },
+    {
+        "mid": 4,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/14.png",
+        "lat": 37.4259498,
+        "lng": -122.0760002,
+        "time": 1437190740,
+        "lock": true,
+        "unlock_type": null,
+        "category": "pokemon"
+    },
+    {
+        "mid": 5,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/15.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": false,
+        "unlock_type": null,
+        "category": "pokemon"
+    },
+    {
+        "mid": 6,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/16.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": true,
+        "unlock_type": "password",
+        "category": "pokemon"
+    },
+    {
+        "mid": 7,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/17.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": false,
+        "unlock_type": null,
+        "category": "pokemon"
+    },
+    {
+        "mid": 8,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/18.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": true,
+        "unlock_type": "upload_pic",
+        "category": "pokemon"
+    },
+    {
+        "mid": 9,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/19.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": true,
+        "unlock_type": "upload_pic",
+        "category": "pokemon"
+    },
+    {
+        "mid": 10,
+        "title": "Pokemon !! ",
+        "img_url": "https://raw.githubusercontent.com/LeoHung/linkedin_hack/master/pseudo_api/img/20.png",
+        "lat": 37.4253498,
+        "lng": -122.0765002,
+        "time": 1437190740,
+        "lock": true,
+        "unlock_type": "upload_pic",
+        "category": "pokemon"
+    }
+]
